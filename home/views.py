bd36e86380fcdb655d334b4e691c2b4e6fe4da75
ufrj_sousa <ufrj_sousa@hcpa.edu.br>
2024-11-08 02:50:24 -0300

    Adicião do bootstrap 5.3.3

--- home/views.py
+++ home/views.py
@@ -34,21 +34,24 @@ def custom_404_view(request):
 def lista_produtos(request):
     produtos = Produto.objects
 
-    # ------------ Paginação dos produtos ------------- #
-    paginator = Paginator(produtos.all(), 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
     # ------------- Filtragem dos produtos ------------ #
     nome_produto = request.POST.get('nome', '')
     setor_armazenamento = request.POST.get('setor', '')
-    if nome_produto:
-        produtos = produtos.filter(nome=nome_produto)
-
-    if setor_armazenamento:
-        produtos = produtos.filter(setor_armazenamento=setor_armazenamento)
+    if nome_produto or setor_armazenamento:
+        if nome_produto:
+            produtos = produtos.filter(nome=nome_produto)
+        
+        if setor_armazenamento:
+            produtos = produtos.filter(setor_armazenamento=setor_armazenamento)
+    else:
+       produtos = produtos.all()   
 
     produtos = produtos.order_by('vencimento')
+
+        # ------------ Paginação dos produtos ------------- #
+    paginator = Paginator(produtos, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     return render(
         request,
